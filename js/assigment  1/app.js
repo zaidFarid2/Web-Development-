332feb5d78ee2a58aa693acdf61448af0532d7ee
Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2023-09-11 02:30:14 -0700

    Add files via upload

---
+++ js/assigment  1/app.js
@@ -0,0 +1,31 @@
+var num1 = +prompt("enter any digit");
+var num2 = +prompt("enter any digit");
+
+// var opr = prompt("Enter any operator  +, - , * , /");
+var div = num1 / num2
+var add = num1 + num2
+var sub = num1 - num2
+var Multiplication1 = num1 * num2
+
+
+document.write(
+    "<table border='1px'  >"+
+    "<tr>"+
+    "<th>"+"Addition"+"</th>"
+    +"<th>"+"Division"+"</th>"
+    +"<th>"+"Subtraction"+"</th>"
+    +"<th>"+"Multiplication"+"</th>"
+    
+    +"</tr>"
+    +"<tr>"+
+    "<td>"+add+"</td>"
+    +"<td>"+Multiplication1+"</td>"
+    +"<td>"+div+"</td>"
+    +"<td>"+sub+"</td>"
+    +"</tr>"+
+   
+     
+    +"</table>"
+)
+    
+
