23bfb463a4b2ccfefb3a18cd338fa6273be18594
Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2023-09-13 01:23:31 -0700

    Add files via upload

---
+++ js/chapter26 to 30/app.js
@@ -0,0 +1,141 @@
+// 1. Write a program that takes a positive integer from user &
+// display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// var user = prompt("enter any positive integer")
+// var first = Math.round(user)
+// document.write("roundoff is " +first +"<br>")
+// var second = Math.ceil(user)
+// document.write("ciel is " +second +"<br>")
+// var third = Math.floor(user)
+// document.write("floor is " +third +"<br>")
+
+
+
+
+
+// 2. Write a program that takes a negative floating point
+// number from user & display the following in your browser.
+// a. number
+// b. round off value of the number
+// c. floor value of the number
+// d. ceil value of the number
+
+// var user = +prompt("enter any negative number") 
+// var first = Math.round(user)
+// document.write("round off is" +first +"<br>")
+// var second = Math.ceil(user)
+// document.write("ciel is" +second +"<br>")
+// var third  = Math.floor(user)
+// document.write("floor is" +third +"<br>")
+ 
+
+
+
+
+
+// 3. Write a program that displays the absolute value of a 
+// number.
+// E.g. absolute value of -4 is 4 & absolute value of 5 is 5
+
+
+
+
+// var user = "-65"
+
+// var letter = user.replace("-","")
+// console.log(letter)
+
+
+
+
+
+
+
+// 4. Write a program that simulates a dice using random() 
+// method of JS Math class. Display the value of dice in your 
+// browser.:
+
+// var user = Math.random()*6
+// var letter  = Math.round(user)
+// console.log("value of dice is "+letter)
+// console.log("value of dice is "+letter)
+
+
+
+
+
+
+// 5. Write a program that simulates a coin toss using random()
+// method of JS Math class. Display the value of coin in your
+// browser
+
+// var userInput= prompt("Choose (0) refers to Head or (1) refers to tail  ")
+// var value = Math.random()*1
+// var round = Math.round(value)
+// if (round == userInput){
+  
+//     document.write("CONGRATS   <br>head win " )
+    
+// }else {
+
+    
+//     document.write("congrat   <br>Tail win")
+// }
+
+
+
+
+
+
+
+// 6. Write a program that shows a random number between 1 
+// and 100 in your browser.
+ 
+// var first  = Math.random()*100
+// var second = Math.round(first)
+// document.write("random number between 1 to  100 is<br> " +second)
+
+  
+
+
+
+
+
+// 7. Write a program that asks the user about his weight. Parse 
+// the user input and display his weight in your browser. 
+// Possible user inputs can be:
+// a. 50
+// b. 50kgs
+// c. 50.2kgs
+// d. 50.2kilograms
+
+
+// var user = prompt("enter  your weight in kilograms")
+// var second = Number(user)
+// document.write(second)
+// console.log(second)
+
+
+
+
+
+
+
+
+// 8. Write a program that stores a random secret number from 
+// 1 to 10 in a variable. Ask the user to input a number 
+// between 1 and 10. If the user input equals the secret 
+// number, congratulate the user
+
+// var user = prompt("Gauess a digit 1 to 10")
+// var first = Math.random()*10
+// var second = Math.round(first)
+// if  (user === second){
+//     document.write("congrats your guess is correct")
+// }else {
+//     document.write("please try again")
+// }
