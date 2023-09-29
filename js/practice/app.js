62ce922b3a60fb9483d04e5951c193febebacc40
Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2023-09-29 22:56:40 +0500

    Add files via upload

---
+++ js/practice/app.js
@@ -0,0 +1,27 @@
+// var user  = "zaid farid"
+// var firstLetter  = user[0].toUpperCase()
+// var secondLetter = user.slice(1,4)
+// var thirdLetter = user[5].toUpperCase()
+// var fourthLetter = user.slice(6)
+// console.log(firstLetter+secondLetter+thirdLetter+fourthLetter) 
+
+
+
+// var user  = prompt("enter your city name")
+// var array = ["karachi", "Mulatan","Peshawar","Quetta", "Sakkhar","Punjab","Balochistan","Kpk","Hyderabad"]
+// var flag = "no"
+// for (var i = 0; i<user.length;i++){
+//     var arrayLower = array[i].toLowerCase()
+//     var userLower = user.toLowerCase()
+//     if (arrayLower === userLower){
+//         flag = "yes"
+//     break
+//     }
+// }
+// if (flag == "yes "){
+//     alert("your city is mentioned in list")
+// }else if (flag == "no"){
+//     alert("city is not found")
+// }else  {
+//     alert("please enter valid city")
+// }
