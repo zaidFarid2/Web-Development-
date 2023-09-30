972ce123782903ebe04f86ac77ef2215aa6ea305
Zaid Farid <137697857+zaidFarid2@users.noreply.github.com>
2023-10-01 02:08:26 +0500

    Add files via upload

---
+++ js/task 3/app.js
@@ -0,0 +1,8 @@
+let Name = prompt("Enter your Name")
+let Password = prompt("Enter your Password")
+if(Name == "zaid"  && Password == "123456"){
+    document.write("Login Successfully")
+}
+else{
+    document.write("Sai dal kanjra")
+}
